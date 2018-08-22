7daef6b2df576c64caec63a1b64ba9ea45697fa1
Mirim_Lee <leemirim9654@gmail.com>
2018-08-23 00:22:03 +0900

    Register ChattyUser

--- chatty_back/chatty_users/admin.py
+++ chatty_back/chatty_users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.ChattyUser)
+class ChattyUserAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'name',
+        'unique_user_id',
+        'partner',
+    )
+
+    list_display_links = (
+        'id',
+    )
