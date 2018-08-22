47d2287a5b985f38ea142793d41e436ff93088bc
Mirim_Lee <leemirim9654@gmail.com>
2018-08-23 00:19:39 +0900

    Register Partner App

--- chatty_back/partners/admin.py
+++ chatty_back/partners/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Partner)
+class PartnerAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'name',
+        'days_together',
+        'diary_count',
+        'creator',
+    )    
