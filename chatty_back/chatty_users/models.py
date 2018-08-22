610a0e16281ddc6e1dd9296fb05e0ff2a0103e8d
Mirim_Lee <leemirim9654@gmail.com>
2018-08-23 00:22:00 +0900

    Add partners fields to ChattyUser

--- chatty_back/chatty_users/models.py
+++ chatty_back/chatty_users/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 
+
 class ChattyUser(models.Model):
 
     """ Chatty User Model """
 
     name = models.CharField(null=True, max_length=80)
     unique_user_id = models.CharField(null=True, max_length=40)
+    partner = models.ForeignKey('partners.Partner', on_delete=models.CASCADE, related_name="partners", null=True, blank=True)
+
 
     def __str__(self):
         return self.name
