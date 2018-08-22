acd3a8943e6c4f184cec835d24d2d2212970b39f
Mirim_Lee <leemirim9654@gmail.com>
2018-08-23 00:19:16 +0900

    Make Partner seirlizers.py

---
+++ chatty_back/partners/serializers.py
@@ -0,0 +1,68 @@
+from rest_framework import serializers
+from . import models
+from chatty_back.chatty_users import serializers as chattyuser_serializers
+from chatty_back.chatty_users import models as chattyuser_models
+
+
+class PartnerProfileSerializer(serializers.ModelSerializer):
+
+    diary_count = serializers.ReadOnlyField()
+    days_together = serializers.ReadOnlyField()
+    created_at = serializers.ReadOnlyField()
+
+    class Meta:
+        model = models.Partner
+        fields = (
+            'id',
+            'profile_image',
+            'name',
+            'bio',
+            'diary_count',
+            'days_together',
+            'created_at',
+        )
+
+
+class CreatePartnerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Partner
+        fields = (
+            'profile_image',
+            'name', 
+            'bio',
+        )
+        extra_kwargs = {'name': {'required': True}}
+
+
+class PartnerListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Partner
+        fields = (
+            'id',
+            'profile_image',
+            'name',
+            'created_at',
+        )
+
+
+#class SetPartnerSerializer(serializers.ModelSerializer):
+
+    #class Meta:
+        #model = chattyuser_models.ChattyUser
+        #fields = (
+        #    'partner',
+        #)
+
+
+class MainPartnerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Partner
+        fields = (
+            'id',
+            'name',
+            'days_together',
+            'diary_count',
+        )
