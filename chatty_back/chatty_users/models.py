e1f1b837ecf75f8e3b49be30095b025ebfb8ff13
Mirim_Lee <leemirim9654@gmail.com>
2018-08-22 23:04:11 +0900

    Create ChattyUser models

--- chatty_back/chatty_users/models.py
+++ chatty_back/chatty_users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class ChattyUser(models.Model):
+
+    """ Chatty User Model """
+
+    name = models.CharField(null=True, max_length=80)
+    unique_user_id = models.CharField(null=True, max_length=40)
+
+    def __str__(self):
+        return self.name
