9b335f8a61e083ab833e5194914e1b6be071e833
Mirim_Lee <leemirim9654@gmail.com>
2018-08-24 23:28:07 +0900

    Make chatty_users urls.py

---
+++ chatty_back/chatty_users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+app_name = "chatty_users"
+urlpatterns = [
+    #path('', views.Main.as_view(), name='diary feed'),
+    path("newuser/", views.NewUser.as_view(), name='first login user'), #hash값 처음으로 반환
+]
