c76c9387856cf063dfb23ff07b6d7a7ccd43570c
Mirim_Lee <leemirim9654@gmail.com>
2018-08-24 23:27:27 +0900

    Make partners urls.py

---
+++ chatty_back/partners/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "partners"
+urlpatterns = [
+    path('', views.PartnerList.as_view(), name='partner list'),
+    path('partner/', views.Partner.as_view(), name='create partner'),
+    path('partner/<int:partner_id>/', views.DeletePartner.as_view(), name='delete partner'),
+    path('profile/<int:partner_id>/', views.PartnerProfile.as_view(), name='get partner profile, modify partner profile'),
+    path('mypartner/<int:partner_id>/', views.SetPartner.as_view(), name='set partner'),
+]
