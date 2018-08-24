f06bdaaa4c84a7457999947be077d4bdd88f603b
Mirim_Lee <leemirim9654@gmail.com>
2018-08-24 18:55:19 +0900

    Make partners views.py

--- chatty_back/partners/views.py
+++ chatty_back/partners/views.py
@@ -1,3 +1,133 @@
-from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . import models, serializers
+from chatty.diary.views import check_user
 
-# Create your views here.
+
+def get_partner(self, partner_id, creator):
+
+    try:
+        found_partner = models.Partner.objects.get(id=partner_id, creator=creator)
+        return found_partner
+    except models.Partner.DoesNotExist:
+        return None
+
+
+class PartnerList(APIView):
+
+    @method_decorator(check_user())
+    def get(selg, request, user, format=None):
+
+        partners = models.Partner.objects.filter(creator=user)
+
+        serializer = serializers.PartnerListSerializer(partners, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class Partner(APIView):
+
+    @method_decorator(check_user())
+    def post(self, request, user, format=None):
+
+        new_partner_name = request.data.get('name', None)
+        
+        try:
+            found_partner = models.Partner.objects.get(name=new_partner_name, creator=user)
+            
+        except models.Partner.DoesNotExist:
+        
+            serializer = serializers.CreatePartnerSerializer(data=request.data, partial=True)
+
+            if serializer.is_valid():
+
+                serializer.save(creator=user)
+
+                return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+                
+            else:
+
+                return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+class PartnerProfile(APIView):
+
+    @method_decorator(check_user())
+    def get(self, request, user, partner_id, format=None):
+
+        found_partner = get_partner(self, partner_id, user)
+
+        if found_partner is None:
+
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        else: 
+
+            serializer = serializers.PartnerProfileSerializer(found_partner)
+
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+    @method_decorator(check_user())
+    def put(self, request, user, partner_id, format=None):
+
+        found_partner = get_partner(self, partner_id, user)
+
+        if found_partner is None:
+
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        else:
+
+            serializer = serializers.PartnerProfileSerializer(
+                found_partner, data=request.data, partial=True)
+
+            if serializer.is_valid():
+
+                serializer.save()
+
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+            else:
+                
+                return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeletePartner(APIView):
+    
+    @method_decorator(check_user())
+    def delete(self, request, user, partner_id, format=None):
+        
+        found_partner = get_partner(self, partner_id, user)
+
+        if found_partner is None:
+
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        else:
+
+            found_partner.delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class SetPartner(APIView):
+
+    @method_decorator(check_user())
+    def put(self, request, user, partner_id, format=None):
+
+        found_partner = get_partner(self, partner_id, user)
+        
+        if found_partner is None:
+
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        else:
+
+            user.partner = found_partner.name
+
+            user.save()
+
+            return Response(status=status.HTTP_200_OK)
