a0385ee31a1d20e91edccfbd1880c09180bdfc38
Mirim_Lee <leemirim9654@gmail.com>
2018-08-25 22:04:35 +0900

    Make diary admin.py

--- chatty_back/diary/admin.py
+++ chatty_back/diary/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+
+    list_display =(
+        'id',
+        'message',
+        'creator',
+    )
+
+@admin.register(models.Question_set)
+class Question_setAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'question_list',
+    )
+
+@admin.register(models.Single_diary)
+class Single_diaryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'state',
+        'creator',
+        'question_set',
+        'created_at',
+    )
+
+
+@admin.register(models.User_answer)
+class User_answerAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'answer',
+        'diary',
+        'creator',
+    )
